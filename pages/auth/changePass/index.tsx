e84dad42abb142bb66e8f767891f5f7c3d747a38
rohanfizz <rohans0501@gmail.com>
2022-01-30 21:51:19 +0530

    last

--- pages/auth/changePass/index.tsx
+++ pages/auth/changePass/index.tsx
@@ -1,12 +1,12 @@
 import ChangePasswordForm from "../../../Components/Auth/ChangePasswordForm";
 import CredentialContainer from "../../../Components/Auth/CredentialContainer";
 import { useRouter } from "next/router";
-import { useSelector } from "react-redux";
+import { RootStateOrAny, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 const ChangePasswordPage: React.FC = (props) => {
 	const router = useRouter();
-	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+	const isLoggedIn = useSelector((state:RootStateOrAny) => state.auth.isLoggedIn);
 	
 	useEffect(() => {
 		if (!isLoggedIn) {
